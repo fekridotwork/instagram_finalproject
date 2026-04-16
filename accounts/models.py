f5b9afe461b33c9988788fce333062224bcb5ba7
Matin Fekri <fekridotwork@gmail.com>
2026-04-16 14:33:43 +0330

    feat: add custom user model and initial accounts migration

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(
+        unique=True,
+        null=True,
+        blank=True
+    )
+    phone_number = models.CharField(
+        max_length=20,
+        unique=True,
+        null=True,
+        blank=True
+    )
+    is_email_verified = models.BooleanField(
+        default=False,
+    )
+    is_phone_verified = models.BooleanField(
+        default=False,
+    )
+
+    def __str__(self):
+        return self.email or self.phone_number or self.username
