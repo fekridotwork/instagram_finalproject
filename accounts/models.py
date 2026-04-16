4c8e7b5d7d5a7f676ae689bac16a4a78a2052379
Matin Fekri <fekridotwork@gmail.com>
2026-04-16 23:12:23 +0330

    feat: add profile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,5 @@
+from email.policy import default
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -22,3 +24,36 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email or self.phone_number or self.username
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        "accounts.User",
+        on_delete= models.CASCADE,
+        related_name="profile"
+    )
+    full_name = models.CharField(
+        max_length=100,
+    )
+    display_name = models.CharField(
+        max_length=50,
+    )
+    bio = models.TextField(
+        blank=True,
+        null=True,
+    )
+    profile_image = models.ImageField(
+        upload_to="profiles/",
+        blank=True,
+        null=True,
+    )
+    is_private = models.BooleanField(
+        default=False
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+    def __str__(self):
+        return self.display_name or self.full_name or str(self.user)
