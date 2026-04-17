5710f312fc019de273ef19b19b42efde5489ac56
Matin Fekri <fekridotwork@gmail.com>
2026-04-17 12:10:00 +0330

    feat: add post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    MEDIA_TYPE_CHOICES = [
+        ('image', 'Image'),
+        ('video', 'Video'),
+    ]
+    VISIBILITY_CHOICES = [
+        ('public', 'Public'),
+        ('followers', 'Followers'),
+        ('close_friends', 'Close_friends'),
+    ]
+    user = models.ForeignKey(
+        "accounts.User",
+        on_delete=models.CASCADE,
+        related_name="posts",
+    )
+    media = models.FileField(
+        upload_to="posts/",
+    )
+    media_type = models.CharField(
+        max_length=10,
+        choices=MEDIA_TYPE_CHOICES,
+    )
+    caption = models.TextField(
+        blank=True,
+    )
+    visibility = models.CharField(
+        max_length=20,
+        choices=VISIBILITY_CHOICES,
+        default='public',
+    )
+    is_deleted = models.BooleanField(
+        default=False,
+    )
+    is_edited = models.BooleanField(
+        default=False,
+    )
+
+    likes_count = models.PositiveIntegerField(
+        default=0,
+    )
+    comments_count = models.PositiveIntegerField(
+        default=0,
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"Post #{self.pk} by {self.user}"
