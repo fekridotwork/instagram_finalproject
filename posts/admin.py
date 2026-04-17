bdb45e0818ed8886cb58a16d33be865530ef1d6c
Matin Fekri <fekridotwork@gmail.com>
2026-04-17 15:48:54 +0330

    feat(admin): setup admin panel for all domains

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from .models import Post, Story, Hashtag, PostHashtag
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "media_type",
+        "visibility",
+        "is_deleted",
+        "likes_count",
+        "comments_count",
+        "created_at",
+    )
+    search_fields = ("user__username", "user__email", "caption")
+    list_filter = ("media_type", "visibility", "is_deleted", "is_edited")
+    ordering = ("-created_at",)
+
+
+@admin.register(Story)
+class StoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "media_type",
+        "visibility",
+        "is_deleted",
+        "expires_at",
+        "created_at",
+    )
+    search_fields = ("user__username", "user__email", "text")
+    list_filter = ("media_type", "visibility", "is_deleted")
+    ordering = ("-created_at",)
+
+
+@admin.register(Hashtag)
+class HashtagAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+    ordering = ("name",)
+
+
+@admin.register(PostHashtag)
+class PostHashtagAdmin(admin.ModelAdmin):
+    list_display = ("id", "post", "hashtag", "created_at")
+    search_fields = ("hashtag__name", "post__user__username", "post__user__email")
+    ordering = ("-created_at",)
