663daa014c6fa23f2e855103c2b792c47acf9a4a
Matin Fekri <fekridotwork@gmail.com>
2026-05-01 12:05:28 +0330

    fix(comment): fix and complete create comment endpoint

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from .views import PostListCreateAPIView, PostDetailAPIView
-from interactions.views import PostLikeAPIView
+from interactions.views import PostLikeAPIView, CommentListCreateAPIView
 
 urlpatterns = [
     path('', PostListCreateAPIView.as_view(), name="post-list-create"),
     path('<int:post_id>/', PostDetailAPIView.as_view(), name="post-detail"),
     path('<int:post_id>/like/', PostLikeAPIView.as_view(), name="post-like"),
+    path('<int:post_id>/comments/', CommentListCreateAPIView.as_view(), name="post-comment"),
 ]
